d2c9c4d47a5900b346681cbf4c81eaf821d0dae1
zhiping.feng <zhiping.feng@dreamsmart.com>
2026-04-22 10:55:00 +0800

    send audio from mcp immediately if is processing audio

--- main/application.cc
+++ main/application.cc
@@ -900,6 +900,11 @@ void Application::HandleStateChangedEvent() {
                 }
                 audio_service_.EnableVoiceProcessing(true);
                 audio_service_.EnableWakeWordDetection(false);
+            } else {
+                //正在处理音频，立即发送来自MCP Server的音频
+                while(auto packet = audio_service_.PopFromOpenClawSendQueue()){
+                    protocol_->SendAudio(std::move(packet));
+                }
             }
 
             // Play popup sound after ResetDecoder (in EnableVoiceProcessing) has been called
